a01850aa77a449f8eebdeaa09b5b037d8f3f6a4e
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-05-13 13:29:03 -0400

    reervation.js to check tables is full

--- reservation.js
+++ reservation.js
@@ -27,10 +27,24 @@ reservBtn.addEventListener('click', (e) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      console.log('reservation.html', data);
-      alert(`Reserved: ${data.name}`);
 
-      alert("Sorry you are on the wait list");
+      const tables = response.json("tables");
+      const waitList = response.json("waitList");
+
+      //check if tables are full
+      if (tables.length>5) {
+        waitList.push(data);
+        alert("Sorry you are on the wait list");
+      }
+      else {
+        tables.push(data);
+        alert(`Reserved: ${data.name}`);
+        console.log('reservation.html', data);
+      }
+      
+      
+
+      
     })
     .catch((error) => {
       console.error('Error:', error);
